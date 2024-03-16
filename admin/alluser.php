fa57d912f18767330a988d4c7757647dcd8d0d12
fatma1-a7med <fatmaahmed110099@gmail.com>
2024-03-16 21:26:43 +0200

    updates

--- admin/alluser.php
+++ admin/alluser.php
@@ -10,17 +10,16 @@ exit();
 $user_id = $_SESSION["user_id"];
 $username = $_SESSION["username"];
 $image = $_SESSION["image"];
-include('includes/navbar.php');
-?>
 
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>User and Room Tables</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head>
-
 <body>
 <?php
+include('includes/navbar.php');
 require('../config/dbcon.php');
 $db = new db(); 
 ?>
